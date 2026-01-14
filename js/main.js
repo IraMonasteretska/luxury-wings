c40fe48de22fbb6c1026434ff7cdd138a2747102
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2026-01-14 16:51:09 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,21 +18,46 @@ $(document).ready(function () {
     });
 
     // language
-    $('.header__language').click(function () {
-        $(this).toggleClass('open');
+    $('.header__language span').click(function () {
+        $(this).parent().toggleClass('open');
     })
+
+     $(document).click(function (event) {
+        let $target = $(event.target);
+        if (!$target.closest('.header__language').length) {
+            $('.header__language').removeClass('open');
+        }
+    });
 
     // testimonials
     var swiper = new Swiper(".testimonials-slider", {
-        slidesPerView: 2,
-        spaceBetween: 24,
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
+        speed: 700,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+
+            768: {
+                slidesPerView: 1,
+                spaceBetween: 40,
+            },
+            991: {
+                slidesPerView: 2,
+                spaceBetween: 24,
+            },
+        },
 
     });
 
     // clients
     var swiper = new Swiper(".clients-slider", {
-        slidesPerView: 'auto',
-        spaceBetween: 60,
+        // slidesPerView: 'auto',
+        slidesPerView: 2,
+        spaceBetween: 20,
         loop: true,
         speed: 700,
         navigation: {
@@ -43,6 +68,20 @@ $(document).ready(function () {
             delay: 5000,
             disableOnInteraction: false,
         },
+        breakpoints: {
+            575: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+            },
+            768: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+            },
+            991: {
+                slidesPerView: 5,
+                spaceBetween: 60,
+            },
+        },
     });
 
     // to top
@@ -51,5 +90,43 @@ $(document).ready(function () {
         $('html, body').animate({ scrollTop: 0 }, 600);
     });
 
+    // burger
+
+    function openMenu() {
+        $('body').addClass('hidden');
+        $('.header__nav').addClass('show');
+
+        // створюємо overlay якщо нема
+        if (!$('.menu-overlay').length) {
+            $('<div class="menu-overlay"></div>').appendTo('header');
+        }
+
+        setTimeout(function () {
+            $('.menu-overlay').addClass('active');
+        }, 10);
+    }
+
+    function closeMenu() {
+        $('body').removeClass('hidden');
+        $('.header__nav').removeClass('show');
+
+        $('.menu-overlay').removeClass('active');
+
+        setTimeout(function () {
+            $('.menu-overlay').remove();
+        }, 300);
+    }
+
+    $('.header__burger').on('click', function () {
+        openMenu();
+    });
+
+    $('.closemenu').on('click', function () {
+        closeMenu();
+    });
+
+    $(document).on('click', '.menu-overlay', function () {
+        closeMenu();
+    });
 
 })
